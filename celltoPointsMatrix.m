9dcfdc741c96fd05b04bb5bd5426ce602ac6d3e9
gispda <gispda@qqq.com>
2018-10-23 14:27:56 +0800

    接入視頻的版本

---
+++ celltoPointsMatrix.m
@@ -0,0 +1,14 @@
+function mPxy = celltoPointsMatrix(contour)
+     
+    dim = numel(contour);
+
+    mPxy = zeros(dim,2); 
+  
+    for i=1:dim
+
+      mPxy(i,1)=contour{i}(1);
+      mPxy(i,2)=contour{i}(2);
+    end
+    
+ 
+end
